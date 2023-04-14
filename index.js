a6024e7d18659aa38898ec9ba684a4f259d30be4
malcomowilla <owillamalcom@gmail.com>
2023-04-14 15:18:23 +0300

    slved

--- index.js
+++ index.js
@@ -1,51 +1,117 @@
-function scuberGreetingForFeet(){
+// function scuberGreetingForFeet(){
 
-  // Write your code here!
+//   // Write your code here!
 
 
-  function scuberGreetingForFeet (distance) {
-    if ( distance <= 400){
-      return ( "This one is on me!") 
-    }
-    else if (distance > 2000 && distance < 2500){
-      return ("I will gladly take your thirty bucks.")
-    }else if (distance > 2500){
-      return ("No Can do.");
-    }
+//   function scuberGreetingForFeet (distance) {
+//     if ( distance <= 400){
+//       return ( "This one is on me!") 
+//     }
+//     else if (distance > 2000 && distance < 2500){
+//       return ("I will gladly take your thirty bucks.")
+//     }else if (distance > 2500){
+//       return ("No Can do.");
+//     }
   
+//   }
+  
+
+  
+  
+  
+  
+
+// }
+
+// function ternaryCheckCity(){
+//   // Write your code here!
+//   let reply;
+//   city === 'NYC' ? (reply= "Ok, sounds good.") : (reply = "No go.")
+//   return reply
+
+// }
+
+// function switchOnCharmFromTip(){
+//   // Write your code here!
+//   switch (tip){
+//     case "generous":
+//       return "Thank you so much.";
+
+//       break
+
+//       case "not as generous":
+//         return "Thank you."
+
+//         break
+//         default:
+//           return "Bye."
+
+
+//   }
+// }
+
+// const someNumber = 0
+// let result;
+// function addFive(someNumber) {
+//   let result;
+//   if (someNumber > 0) {
+//     result = someNumber + 5
+//   }
+
+//   console.log(result)
+//   return result
+// }
+
+// addFive(10)
+
+// addFive(addFive(5));
+
+
+
+function scuberGreetingForFeet(ride) {
+  let message;
+  if (ride <= 400) {
+    message = "This one is on me!"
+  } else if (ride > 2000 &&  ride < 2500) {
+    message = "I will gladly take your thirty bucks."
+
+  }  else {
+    message = "No can do."
   }
-  
-
-  
-  
-  
-  
-
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-  let reply;
-  city === 'NYC' ? (reply= "Ok, sounds good.") : (reply = "No go.")
-  return reply
 
+function ternaryCheckCity(city) {
+let compliment;
+ city === "NYC" ? (compliment="Ok, sounds good.") : (compliment="No go.")
+
+ return compliment
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-  switch (tip){
-    case "generous":
+function switchOnCharmFromTip(tip) {
+  // let response
+  switch (tip) {
+    // case "generous":
+    //   response = "Thank you so much."
+    //   case "not as generous":
+    //     response = "Thank you."
+  
+    // default:
+    //   response = "Bye."
+    //   return response
+      case "generous":
       return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
 
-      break
-
-      case "not as generous":
-        return "Thank you."
-
-        break
-        default:
-          return "Bye."
-
-
-  }
+      }
 }
+
+
+
+ 
+
+
